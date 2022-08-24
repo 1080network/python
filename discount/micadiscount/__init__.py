28ca49b0fc69165dab73e2d05523c92c1609c79f
Eduardo Solis <eduardo.solis@mica.io>
2022-08-24 15:44:17 -0500

    proper default address

--- discount/micadiscount/__init__.py
+++ discount/micadiscount/__init__.py
@@ -6,7 +6,7 @@ import micadiscount.discount.service.v1.discount_service_pb2_grpc as discount
 discount_roles = ['RoleDiscountAdmin']
 
 
-def build_test_discount_client(addr: str = '[::]:14100'):
+def build_test_discount_client(addr: str = 'localhost:14100'):
     channel = create_channel(addr=addr, jwt_roles=discount_roles)
     return build_discount_client(channel=channel)
 
